e5fe37c7c88d263e9f73c036313c46d9260139c1
Josh Haawke <joshuathawke@gmail.com>
2023-02-12 14:43:56 -0500

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+const main = document.querySelector("main#main");
+main.parentNode.removeChild(main);
+
+const newHeader = document.createElement("h1");
+
+
+newHeader.id = "victory";
+
+const name = "Hawke";
+newHeader.textContent = `${name} is the champion`;
+
+
+document.body.appendChild(newHeader);
